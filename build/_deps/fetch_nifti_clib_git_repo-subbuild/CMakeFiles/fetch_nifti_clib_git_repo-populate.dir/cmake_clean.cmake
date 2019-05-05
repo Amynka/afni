7bda948ec3b1574a32fe3c0b024bb7a867f54e53
Amy Liffey <amynka@gentoo.org>
2019-05-05 20:14:39 +0000

    Add cmake files

---
+++ build/_deps/fetch_nifti_clib_git_repo-subbuild/CMakeFiles/fetch_nifti_clib_git_repo-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fetch_nifti_clib_git_repo-populate"
+  "CMakeFiles/fetch_nifti_clib_git_repo-populate-complete"
+  "fetch_nifti_clib_git_repo-populate-prefix/src/fetch_nifti_clib_git_repo-populate-stamp/fetch_nifti_clib_git_repo-populate-install"
+  "fetch_nifti_clib_git_repo-populate-prefix/src/fetch_nifti_clib_git_repo-populate-stamp/fetch_nifti_clib_git_repo-populate-mkdir"
+  "fetch_nifti_clib_git_repo-populate-prefix/src/fetch_nifti_clib_git_repo-populate-stamp/fetch_nifti_clib_git_repo-populate-download"
+  "fetch_nifti_clib_git_repo-populate-prefix/src/fetch_nifti_clib_git_repo-populate-stamp/fetch_nifti_clib_git_repo-populate-update"
+  "fetch_nifti_clib_git_repo-populate-prefix/src/fetch_nifti_clib_git_repo-populate-stamp/fetch_nifti_clib_git_repo-populate-patch"
+  "fetch_nifti_clib_git_repo-populate-prefix/src/fetch_nifti_clib_git_repo-populate-stamp/fetch_nifti_clib_git_repo-populate-configure"
+  "fetch_nifti_clib_git_repo-populate-prefix/src/fetch_nifti_clib_git_repo-populate-stamp/fetch_nifti_clib_git_repo-populate-build"
+  "fetch_nifti_clib_git_repo-populate-prefix/src/fetch_nifti_clib_git_repo-populate-stamp/fetch_nifti_clib_git_repo-populate-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/fetch_nifti_clib_git_repo-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/_deps/fetch_testing_data-src/README.md
@@ -0,0 +1,15 @@
+# VERSIONS
+## v3.0.0 data intended for testing `nifti_clib` version 3.0.0
+
+# nifti-test-data
+
+A repository for storing test data used for the nifti project.  This repository is used as a storage location for the `nifti_clib` project initial conversion to github, and may be replaced with a more sustainable/modular solution in the future.
+
+Releases of this data set will provide a link to a tarball that
+can be used inside of the `nifti_clib` project for testing
+the functionality of those programs.
+
+
+## Data copied from [CiftiLib](https://github.com/Washington-University/CiftiLib/tree/master/example/data)
+
+The Washington-University data is a convenience copy in the local diredtory.
