3686d089f53612876ef682f039a1be66006426e8
Amy Liffey <amy.liffey@alpiq.com>
2019-05-25 23:06:54 +0200

    Fix path for Xp cmake

--- cmake/FindXp.cmake
+++ cmake/FindXp.cmake
@@ -5,8 +5,8 @@ find_package(PkgConfig)
 pkg_check_modules(PC QUIET xp)
 set(XP_DEFINITIONS ${PC_CFLAGS_OTHER})
 
-find_path(XP_INCLUDE_DIR X11/extensions/Print.h HINTS ${PC_INCLUDE_DIRS})
-find_library(XP_LIBRARY NAMES Xp HINTS ${PC_LIBRARY_DIRS})
+find_path(XP_INCLUDE_DIR Xm/Print.h HINTS ${PC_INCLUDE_DIRS})
+find_library(XP_LIBRARY NAMES Xm HINTS ${PC_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Xp DEFAULT_MSG XP_LIBRARY XP_INCLUDE_DIR)
